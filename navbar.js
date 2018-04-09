30b351eb0ca903a64e80d85cec27ff6304dfeab0
Elliot Simpson <simpsonj@uncw.edu>
2018-04-09 10:24:18 -0400

    fix styling on search

--- navbar.js
+++ navbar.js
@@ -10428,6 +10428,10 @@ $(document).ready(function() {
 	'    margin-bottom: 25px;'+
 	''+
 	'}'+
+	''+
+	'.SearchResult-thumbnailHolder {'+
+	'    margin-right:10px;'+
+	'}'+
 	'</style>';
 		
 	var bootstrap = '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">'+
